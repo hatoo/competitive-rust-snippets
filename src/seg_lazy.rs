ad5238a04b2519561e8f001ae5e91d9ab0ad7b0e
hatoo <hato2000@gmail.com>
2018-01-21 13:29:50 +0900

    Fix lazy segtree

--- src/seg_lazy.rs
+++ src/seg_lazy.rs
@@ -12,10 +12,7 @@ struct SEG<T: SEGimpl> {
 impl<T: SEGimpl> SEG<T> {
     #[allow(dead_code)]
     fn new(n: usize, init: T::Elem) -> SEG<T> {
-        let n = (1..)
-            .map(|i| 2usize.pow(i as u32))
-            .find(|&x| x >= n)
-            .unwrap();
+        let n = (1..).map(|i| 1 << i).find(|&x| x >= n).unwrap();
         SEG {
             n: n,
             buf: vec![init; 2 * n],
@@ -25,16 +22,6 @@ impl<T: SEGimpl> SEG<T> {
 
     #[allow(dead_code)]
     fn eval(&mut self, k: usize, l: usize, r: usize) {
-        //
-        /*
-        let x = self.buf[k].1;
-        self.buf[k].1 = 0;
-        self.buf[k].0 += x;
-        if r - l > 1 {
-            self.buf[2 * k + 1].1 += x / 2;
-            self.buf[2 * k + 2].1 += x / 2;
-        }
-        */
         if r - l > 1 {
             let (l, r) = self.buf.split_at_mut(2 * k + 1);
             let (c1, c2) = r.split_at_mut(1);
@@ -45,21 +32,28 @@ impl<T: SEGimpl> SEG<T> {
     }
 
     #[allow(dead_code)]
-    fn range_add(&mut self, x: &T::A, a: usize, b: usize, k: usize, l: usize, r: usize) {
+    fn r(&mut self, x: &T::A, a: usize, b: usize, k: usize, l: usize, r: usize) {
         self.eval(k, l, r);
         if r <= a || b <= l {
             return;
         }
         if a <= l && r <= b {
-            //
-            // self.buf[k].1 += (r - l) as u64 * x;
             T::range(x, &mut self.buf[k], l, r);
             self.eval(k, l, r);
             return;
         }
 
-        self.range_add(x, a, b, 2 * k + 1, l, (l + r) / 2);
-        self.range_add(x, a, b, 2 * k + 2, (l + r) / 2, r);
+        self.r(x, a, b, 2 * k + 1, l, (l + r) / 2);
+        self.r(x, a, b, 2 * k + 2, (l + r) / 2, r);
+        let (l, r) = self.buf.split_at_mut(2 * k + 1);
+        let (c1, c2) = r.split_at_mut(1);
+        T::reduce(&mut l[k], &c1[0], &c2[0]);
+    }
+
+    #[allow(dead_code)]
+    fn range_add(&mut self, x: &T::A, a: usize, b: usize) {
+        let n = self.n;
+        self.r(x, a, b, 0, 0, n);
     }
 
     #[allow(dead_code)]
@@ -74,7 +68,7 @@ impl<T: SEGimpl> SEG<T> {
             let vl = self.q(a, b, k * 2 + 1, l, (l + r) / 2);
             let vr = self.q(a, b, k * 2 + 2, (l + r) / 2, r);
             match (vl, vr) {
-                (Some(l), Some(r)) => Some(T::reduce(l, r)),
+                (Some(l), Some(r)) => Some(T::reduce_result(l, r)),
                 (Some(l), None) => Some(l),
                 (None, Some(r)) => Some(r),
                 _ => None,
@@ -96,6 +90,66 @@ trait SEGimpl {
 
     fn eval(parent: &mut Self::Elem, children: Option<(&mut Self::Elem, &mut Self::Elem)>);
     fn range(x: &Self::A, elem: &mut Self::Elem, l: usize, r: usize);
+    fn reduce(parent: &mut Self::Elem, c1: &Self::Elem, c2: &Self::Elem);
     fn result(elem: &Self::Elem) -> Self::R;
-    fn reduce(a: Self::R, b: Self::R) -> Self::R;
+    fn reduce_result(a: Self::R, b: Self::R) -> Self::R;
+}
+
+#[allow(dead_code)]
+struct RangeAddSum();
+impl SEGimpl for RangeAddSum {
+    type Elem = (u64, u64);
+    type A = u64;
+    type R = u64;
+
+    fn eval(parent: &mut Self::Elem, children: Option<(&mut Self::Elem, &mut Self::Elem)>) {
+        let x = parent.1;
+        parent.0 += x;
+        parent.1 = 0;
+        if let Some((c1, c2)) = children {
+            c1.1 += x / 2;
+            c2.1 += x / 2;
+        }
+    }
+    fn range(x: &Self::A, elem: &mut Self::Elem, l: usize, r: usize) {
+        elem.1 += (r - l) as u64 * x;
+    }
+    fn result(elem: &Self::Elem) -> Self::R {
+        elem.0
+    }
+    fn reduce(parent: &mut Self::Elem, c1: &Self::Elem, c2: &Self::Elem) {
+        parent.0 = c1.0 + c2.0;
+    }
+    fn reduce_result(a: Self::R, b: Self::R) -> Self::R {
+        a + b
+    }
+}
+
+#[test]
+fn test_seg_lazy() {
+    use util;
+    use rand::{Rng, SeedableRng, StdRng};
+
+    let size = 1000;
+    let mut v = vec![0; size];
+    let mut seg: SEG<RangeAddSum> = SEG::new(size, (0, 0));
+    let mut rng = StdRng::from_seed(&[1, 2, 3, 4, 5]);
+
+    for _ in 0..1000 {
+        let x = rng.next_u64() % 256;
+        let r = util::random_range(&mut rng, 0, size);
+        seg.range_add(&x, r.start, r.end);
+        for i in r {
+            v[i] += x;
+        }
+
+        let r = util::random_range(&mut rng, 0, size);
+        let seg_sum = seg.query(r.start, r.end).unwrap_or(0);
+        let mut sum = 0;
+        for i in r {
+            sum += v[i];
+        }
+
+        assert_eq!(seg_sum, sum);
+    }
 }
