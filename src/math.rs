3f61484b5bf166620246316491bd7671ecd93357
hatoo <hato2000@gmail.com>
2018-01-23 17:21:30 +0900

    Update convex hull check function interface

--- src/math.rs
+++ src/math.rs
@@ -18,9 +18,18 @@ fn partition_dp(n: usize, m: usize, p: u64) -> Vec<Vec<u64>> {
 }
 
 #[snippet = "convex_hull_check"]
+type ConvexHullType = i64;
+
+#[snippet = "convex_hull_check"]
 #[allow(dead_code)]
-fn convex_hull_check((a1, b1): (f64, f64), (a2, b2): (f64, f64), (a3, b3): (f64, f64)) -> bool {
-    (a2 - a1) * (b3 - b2) >= (b2 - b1) * (a3 - a2)
+fn convex_hull_check(
+    (a1, b1): (ConvexHullType, ConvexHullType),
+    (a2, b2): (ConvexHullType, ConvexHullType),
+    (a3, b3): (ConvexHullType, ConvexHullType),
+) -> bool {
+    // Convert to f64 due to overflow
+    (a2 as f64 - a1 as f64) * (b3 as f64 - b2 as f64)
+        >= (b2 as f64 - b1 as f64) * (a3 as f64 - a2 as f64)
 }
 
 #[test]
