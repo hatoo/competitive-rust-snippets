907552116964f46acbe8710a54ae644eb1d8a3f1
hatoo <hato2000@gmail.com>
2018-01-23 16:41:44 +0900

    Add utility function to convex hull trick

--- src/math.rs
+++ src/math.rs
@@ -17,6 +17,12 @@ fn partition_dp(n: usize, m: usize, p: u64) -> Vec<Vec<u64>> {
     dp
 }
 
+#[snippet = "convex_hull_check"]
+#[allow(dead_code)]
+fn convex_hull_check((a1, b1): (f64, f64), (a2, b2): (f64, f64), (a3, b3): (f64, f64)) -> bool {
+    (a2 - a1) * (b3 - b2) >= (b2 - b1) * (a3 - a2)
+}
+
 #[test]
 fn test_partition_dp() {
     const M: u64 = 1000000007;
