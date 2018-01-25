d418014fb1a0a9df6f897b3ba583816da6e6850a
hatoo <hato2000@gmail.com>
2018-01-25 23:46:28 +0900

    Fix with_bufwriter

--- src/io.rs
+++ src/io.rs
@@ -2,9 +2,10 @@ use std;
 
 // println! is very slow when print more than 10^5 lines.
 #[snippet = "with_bufwriter"]
+#[allow(dead_code)]
 fn with_bufwriter<F: FnOnce(std::io::BufWriter<std::io::StdoutLock>) -> ()>(f: F) {
     let out = std::io::stdout();
-    let mut writer = std::io::BufWriter::new(out.lock());
+    let writer = std::io::BufWriter::new(out.lock());
     f(writer)
 }
 
@@ -13,6 +14,6 @@ fn test_with_bufwriter() {
     // sample code
     use std::io::Write;
     with_bufwriter(|mut out| {
-        writeln!(out, "ok");
+        writeln!(out, "ok").unwrap();
     });
 }
