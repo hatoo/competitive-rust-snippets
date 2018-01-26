09ebac5b8072299d1b4f7205222f91e6924054bf
hatoo <hato2000@gmail.com>
2018-01-27 03:04:04 +0900

    Update convex_hull_check
    - i64 is ok for all most all cases

--- src/math.rs
+++ src/math.rs
@@ -19,15 +19,8 @@ fn partition_dp(n: usize, m: usize, p: u64) -> Vec<Vec<u64>> {
 }
 
 #[snippet = "convex_hull_check"]
-type ConvexHullType = i64;
-
-#[snippet = "convex_hull_check"]
 #[allow(dead_code)]
-fn convex_hull_check(
-    (a1, b1): (ConvexHullType, ConvexHullType),
-    (a2, b2): (ConvexHullType, ConvexHullType),
-    (a3, b3): (ConvexHullType, ConvexHullType),
-) -> bool {
+fn convex_hull_check((a1, b1): (i64, i64), (a2, b2): (i64, i64), (a3, b3): (i64, i64)) -> bool {
     // Convert to f64 due to overflow
     (a2 as f64 - a1 as f64) * (b3 as f64 - b2 as f64)
         >= (b2 as f64 - b1 as f64) * (a3 as f64 - a2 as f64)
