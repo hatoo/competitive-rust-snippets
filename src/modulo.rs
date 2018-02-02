da2283142e8425d040dea3ca9099986233b8ea2b
hatoo <hato2000@gmail.com>
2018-02-02 15:39:44 +0900

    Add fact and inverse fact table initialization

--- src/modulo.rs
+++ src/modulo.rs
@@ -61,6 +61,25 @@ pub fn fact_table(len: usize, m: u64) -> Vec<u64> {
 
 #[snippet = "mod"]
 #[allow(dead_code)]
+/// Factorial and Inverse factorial table
+pub fn fact_inv_table(size: usize, m: u64) -> (Vec<u64>, Vec<u64>) {
+    let mut fact = vec![1; size];
+    let mut fact_inv = vec![1; size];
+
+    for i in 2..size {
+        fact[i] = fact[i - 1] * i as u64 % m;
+        fact_inv[i] = m - ((m / i as u64) * fact_inv[(m % i as u64) as usize] % m);
+    }
+
+    for i in 1..size {
+        fact_inv[i] = fact_inv[i - 1] * fact_inv[i] % m;
+    }
+
+    (fact, fact_inv)
+}
+
+#[snippet = "mod"]
+#[allow(dead_code)]
 /// (a mod p, e when n! = a p\^e)
 pub fn mod_fact(n: u64, p: u64, fact: &[u64]) -> (u64, u64) {
     if n == 0 {
@@ -113,4 +132,15 @@ fn test_mod_comb_repetition() {
     assert_eq!(mod_comb_repetition(10, 4, m, &fact), 715);
     assert_eq!(mod_comb_repetition(400, 296, m, &fact), 546898535);
     assert_eq!(mod_comb_repetition(100000, 100000, m, &fact), 939733670);
+}
+
+#[test]
+fn test_fact_inv_table() {
+    let m = 1_000_000_007;
+    let size = 1000;
+    let (fact, fact_inv) = fact_inv_table(1000, m);
+
+    for i in 0..size {
+        assert_eq!(fact[i] * fact_inv[i] % m, 1);
+    }
 }
