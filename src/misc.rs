6b69a0490c0ff20efeb0384de37a1c11e196467b
hatoo <hato2000@gmail.com>
2018-02-19 12:33:14 +0900

    Add adjecant4/8

--- src/misc.rs
+++ src/misc.rs
@@ -26,3 +26,90 @@ impl<T: Ord, U> Ord for CmpBy<T, U> {
         (self.0).cmp(&other.0)
     }
 }
+
+#[snippet = "adjacent4"]
+#[allow(dead_code)]
+fn adjacent4(x: usize, y: usize, sx: usize, sy: usize) -> Vec<(usize, usize)> {
+    [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        .into_iter()
+        .filter_map(|&(dx, dy)| {
+            let nx = x as isize + dx;
+            let ny = y as isize + dy;
+
+            if nx >= 0 && nx < sx as isize && ny >= 0 && ny < sy as isize {
+                Some((nx as usize, ny as usize))
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+#[snippet = "adjacent8"]
+#[allow(dead_code)]
+fn adjacent8(x: usize, y: usize, sx: usize, sy: usize) -> Vec<(usize, usize)> {
+    [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, -1),
+        (-1, 1),
+        (1, -1),
+        (1, 1),
+    ].into_iter()
+        .filter_map(|&(dx, dy)| {
+            let nx = x as isize + dx;
+            let ny = y as isize + dy;
+
+            if nx >= 0 && nx < sx as isize && ny >= 0 && ny < sy as isize {
+                Some((nx as usize, ny as usize))
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+#[test]
+fn test_adjacent4() {
+    let mut a4 = adjacent4(1, 1, 3, 3);
+    let mut expected = [(0, 1), (2, 1), (1, 0), (1, 2)];
+    a4.sort();
+    expected.sort();
+
+    assert_eq!(&a4, &expected);
+
+    let mut a4 = adjacent4(0, 0, 3, 3);
+    let mut expected = [(1, 0), (0, 1)];
+    a4.sort();
+    expected.sort();
+
+    assert_eq!(&a4, &expected);
+}
+
+#[test]
+fn test_adjacent8() {
+    let mut a8 = adjacent8(1, 1, 3, 3);
+    let mut expected = [
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (1, 0),
+        (1, 2),
+        (2, 0),
+        (2, 1),
+        (2, 2),
+    ];
+    a8.sort();
+    expected.sort();
+
+    assert_eq!(&a8, &expected);
+
+    let mut a8 = adjacent8(0, 0, 3, 3);
+    let mut expected = [(1, 0), (0, 1), (1, 1)];
+    a8.sort();
+    expected.sort();
+
+    assert_eq!(&a8, &expected);
+}
