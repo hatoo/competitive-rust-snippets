05518bc2c4dc1aae93a69f524baf10492ac82d12
hatoo <hato2000@gmail.com>
2018-03-25 18:56:13 +0900

    Add bridges

--- src/graph.rs
+++ src/graph.rs
@@ -297,6 +297,49 @@ pub fn articulation_points(g: &[Vec<usize>]) -> Vec<usize> {
     res
 }
 
+#[snippet = "bridges"]
+pub fn bridges(g: &[Vec<usize>]) -> Vec<(usize, usize)> {
+    fn dfs(
+        i: usize,
+        p: usize,
+        g: &[Vec<usize>],
+        t: usize,
+        visited: &mut [bool],
+        pre: &mut [usize],
+        low: &mut [usize],
+        res: &mut Vec<(usize, usize)>,
+    ) -> usize {
+        visited[i] = true;
+        pre[i] = t;
+        low[i] = t;
+
+        for &to in &g[i] {
+            if p != to {
+                if visited[to] {
+                    low[i] = min(low[i], low[to]);
+                } else {
+                    low[i] = min(low[i], dfs(to, i, g, t + 1, visited, pre, low, res));
+
+                    if low[to] == pre[to] {
+                        res.push((i, to));
+                    }
+                }
+            }
+        }
+
+        low[i]
+    }
+
+    let n = g.len();
+    let mut visited = vec![false; n];
+    let mut pre = vec![0; n];
+    let mut low = vec![0; n];
+    let mut res = Vec::new();
+
+    dfs(0, 0, g, 0, &mut visited, &mut pre, &mut low, &mut res);
+    res
+}
+
 #[test]
 fn test_lca() {
     use rand::{Rng, SeedableRng, StdRng};
