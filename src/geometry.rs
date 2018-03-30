535f194c1908c0b09f0704edce423da61e24768f
hatoo <hato2000@gmail.com>
2018-03-31 00:40:30 +0900

    Add is_intersected

--- src/geometry.rs
+++ src/geometry.rs
@@ -187,6 +187,19 @@ pub fn closest_pair(ps: &[(f64, f64)]) -> ((f64, f64), (f64, f64)) {
     rec(&x_sort, &y_sort)
 }
 
+/// Is line a-b and line c-d intersected ?
+#[snippet = "is_intersected"]
+pub fn is_intersected(a: Vector2D, b: Vector2D, c: Vector2D, d: Vector2D) -> bool {
+    let ta = (c.0 - d.0) * (a.1 - c.1) + (c.1 - d.1) * (c.0 - a.0);
+    let tb = (c.0 - d.0) * (b.1 - c.1) + (c.1 - d.1) * (c.0 - b.0);
+    let tc = (a.0 - b.0) * (c.1 - a.1) + (a.1 - b.1) * (a.0 - c.0);
+    let td = (a.0 - b.0) * (d.1 - a.1) + (a.1 - b.1) * (a.0 - d.0);
+
+    tc * td <= 0.0 && ta * tb <= 0.0
+    // Not intersects start or end point.
+    // tc * td < 0.0 && ta * tb < 0.0
+}
+
 #[test]
 fn test_convex_hull() {
     let vs = vec![
