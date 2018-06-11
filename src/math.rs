5b6bff04e74045766595421b680f7175f209ae3b
hatoo <hato2000@gmail.com>
2018-06-11 11:14:30 +0900

    Add nck_prob

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,24 @@
+#[snippet]
+#[allow(dead_code)]
+/// v[n][k] = nCk / 2^n
+fn nck_prob(n: usize) -> Vec<Vec<f64>> {
+    let mut res = vec![vec![1.0]];
+
+    for _ in 1..n {
+        let mut v = Vec::new();
+        {
+            let last = res.last().unwrap();
+            v.push(last.first().unwrap() / 2.0);
+            for i in 0..last.len() - 1 {
+                v.push((last[i] + last[i + 1]) / 2.0);
+            }
+            v.push(last.last().unwrap() / 2.0);
+        }
+        res.push(v);
+    }
+    res
+}
+
 #[snippet = "partition_dp"]
 #[allow(dead_code)]
 /// dp[i][j] = j th partition number of i
