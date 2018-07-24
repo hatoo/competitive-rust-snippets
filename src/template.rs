ce29f23368190c569920c887f4df45e6ff7246cc
hatoo <hato2000@gmail.com>
2018-07-24 20:12:59 +0900

    Fix template
    - Remove broken AA

--- src/template.rs
+++ src/template.rs
@@ -1,31 +1,20 @@
 #![snippet = "template"]
 
-/**
-*  _           _                 __                            _   _ _   _                                 _                    _                  _
-* | |         | |               / /                           | | (_) | (_)                               | |                  (_)                | |
-* | |__   __ _| |_ ___   ___   / /__ ___  _ __ ___  _ __   ___| |_ _| |_ ___   _____ ______ _ __ _   _ ___| |_ ______ ___ _ __  _ _ __  _ __   ___| |_ ___
-* | '_ \ / _\` | __/ _ \ / _ \ / / __/ _ \| '_ \` _ \| '_ \ / _ \ __| | __| \ \ / / _ \______| '__| | | / __| __|______/ __| '_ \| | '_ \| '_ \ / _ \ __/ __|
-* | | | | (_| | || (_) | (_) / / (_| (_) | | | | | | |_) |  __/ |_| | |_| |\ V /  __/      | |  | |_| \__ \ |_       \__ \ | | | | |_) | |_) |  __/ |_\__ \
-* |_| |_|\__,_|\__\___/ \___/_/ \___\___/|_| |_| |_| .__/ \___|\__|_|\__|_| \_/ \___|      |_|   \__,_|___/\__|      |___/_| |_|_| .__/| .__/ \___|\__|___/
-*                                                  | |                                                                           | |   | |
-*                                                  |_|                                                                           |_|   |_|
-*
-* https://github.com/hatoo/competitive-rust-snippets
-*/
+/// https://github.com/hatoo/competitive-rust-snippets
 
 #[allow(unused_imports)]
 use std::cmp::{max, min, Ordering};
 #[allow(unused_imports)]
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
-use std::iter::FromIterator;
-#[allow(unused_imports)]
 use std::io::{stdin, stdout, BufWriter, Write};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
 
 mod util {
+    use std::fmt::Debug;
     use std::io::{stdin, stdout, BufWriter, StdoutLock};
     use std::str::FromStr;
-    use std::fmt::Debug;
 
     #[allow(dead_code)]
     pub fn line() -> String {
