efb283b0e4caf4b862088ebe01686d0b147cdef0
hatoo <hato2000@gmail.com>
2018-08-06 13:39:23 +0900

    Add fibonacci

--- src/modulo.rs
+++ src/modulo.rs
@@ -123,6 +123,49 @@ pub fn mod_comb_repetition(n: u64, k: u64, p: u64, fact: &[u64]) -> u64 {
     mod_comb(n - 1 + k, n - 1, p, fact)
 }
 
+#[snippet]
+#[snippet = "fib"]
+#[allow(dead_code)]
+/// Matrix mul
+fn mat_mul2(x: &[u64; 4], y: &[u64; 4], p: u64) -> [u64; 4] {
+    [
+        (x[0] * y[0] + x[1] * y[2]) % p,
+        (x[0] * y[1] + x[1] * y[3]) % p,
+        (x[2] * y[0] + x[3] * y[2]) % p,
+        (x[2] * y[1] + x[3] * y[3]) % p,
+    ]
+}
+
+#[snippet]
+#[snippet = "fib"]
+#[allow(dead_code)]
+/// Matrix pow
+fn pow_mat(m: &[u64; 4], n: u64, p: u64) -> [u64; 4] {
+    let mut m = m.clone();
+    let mut res = [1, 0, 1, 0];
+
+    for i in 0.. {
+        if n >> i == 0 {
+            break;
+        }
+
+        if n >> i & 1 == 1 {
+            res = mat_mul2(&res, &m, p);
+        }
+        m = mat_mul2(&m, &m, p);
+    }
+
+    res
+}
+
+#[snippet = "fib"]
+#[allow(dead_code)]
+/// Fast fibonacci calculation
+fn fib(i: u64, p: u64) -> u64 {
+    let m = pow_mat(&[1, 1, 1, 0], i, p);
+    m[1]
+}
+
 #[test]
 fn test_mod_comb_repetition() {
     let m = 1_000_000_007;
@@ -155,4 +198,20 @@ fn test_mod_pow() {
         assert_eq!(mod_pow(x, i, m), t);
         t = t * x % m;
     }
+}
+
+#[test]
+fn test_fib() {
+    pub const M: u64 = 1_000_000_007;
+    assert_eq!(fib(0, M), 0);
+    assert_eq!(fib(1, M), 1);
+    assert_eq!(fib(2, M), 1);
+    assert_eq!(fib(3, M), 2);
+    assert_eq!(fib(4, M), 3);
+    assert_eq!(fib(5, M), 5);
+    assert_eq!(fib(6, M), 8);
+    assert_eq!(fib(7, M), 13);
+    assert_eq!(fib(8, M), 21);
+
+    assert_eq!(fib(10000000000, M) * fib(10000000001, M) % M, 128493982);
 }
